84d895ca66c84061e5f35169fa0d9f14656d6854
TajulMunandar <tajulmunandar701@gmail.com>
2024-05-08 10:27:14 +0700

    Front-End: Dashboard Template

--- Front-End/AR/src/App.jsx
+++ Front-End/AR/src/App.jsx
@@ -2,12 +2,17 @@ import { useRoutes } from "react-router-dom";
 
 // route
 import Logins from "./pages/auth/Logins";
+import Dashboard from "./pages/dashboard/dashboard";
 
 function App() {
   const routes = useRoutes([
     {
       path: "/login",
       element: <Logins />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
     },
   ]);
 
